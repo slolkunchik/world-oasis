1627bd47fa9b7f0acc1f903333cc4eef9a554938
Marina Sevkovich <slolkunchik@gmail.com>
2026-02-03 14:05:49 +0300

    feat : add query for cabins

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import toast from "react-hot-toast";
+
+import { formatCurrency } from "../../utils/helpers";
+import { deleteCabin } from "../../services/apiCabins";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +43,38 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image, id } = cabin;
+
+  const queryClient = useQueryClient();
+  const { isPending: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      toast.success("Cabin succesfully deleted");
+
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} quests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button
+        disabled={isDeleting}
+        onClick={() => {
+          mutate(id);
+        }}
+      >
+        Delete
+      </button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,19 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable.jsx";
 
 function Cabins() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter/sort</p>
+      </Row>
+
+      <Row type="vertical">
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
