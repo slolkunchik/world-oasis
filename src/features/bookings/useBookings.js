5091cc476329d38944acf5a8de5fe66e264ddddb
Marina Sevkovich <slolkunchik@gmail.com>
2026-02-09 16:23:26 +0300

    feat: add backend filtering

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,11 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
 
 import { getBookings } from "../../services/apiBookings";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  //{ field: "totalPrice", value: 5000 , method: "gte" }
+
   const { isPending, data: bookings } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isPending, bookings };
