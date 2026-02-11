dd349620c84776a4608caedb3c73a9050989c1f3
Marina Sevkovich <slolkunchik@gmail.com>
2026-02-11 16:11:23 +0300

    feat: authorization

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router";
 
@@ -6,10 +6,12 @@ import { login as loginApi } from "../../services/apiAuth";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isPending } = useMutation({
     mutationFn: (email, password) => loginApi(email, password),
-    onSuccess: () => {
+    onSuccess: (data) => {
+      queryClient.setQueryData(["user"], data.user);
       navigate("/dashboard");
     },
     onError: (error) => {
