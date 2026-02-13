77eb0887d8a2caa5684e7ef1e7b4acd250bbde09
Marina Sevkovich <slolkunchik@gmail.com>
2026-02-13 13:38:30 +0300

    feat: add prebuild email and password

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("marina@gmail.com");
+  const [password, setPassword] = useState("12345678");
   const { login, isPending } = useLogin();
 
   function handleSubmit(e) {
