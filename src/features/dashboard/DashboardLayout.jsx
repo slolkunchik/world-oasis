093188a24c61eac64280811a69e5940a33218629
Marina Sevkovich <slolkunchik@gmail.com>
2026-02-17 16:54:20 +0300

    feat: add duration chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,7 +34,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       ></Stats>
       <div>Today's activity</div>
-      <div>Chart stay durations</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -1,4 +1,8 @@
+import { Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 import styled from "styled-components";
+
+import Heading from "../../ui/Heading";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 const ChartBox = styled.div`
   /* Box */
@@ -22,42 +26,42 @@ const startDataLight = [
   {
     duration: "1 night",
     value: 0,
-    color: "#ef4444",
+    fill: "#ef4444",
   },
   {
     duration: "2 nights",
     value: 0,
-    color: "#f97316",
+    fill: "#f97316",
   },
   {
     duration: "3 nights",
     value: 0,
-    color: "#eab308",
+    fill: "#eab308",
   },
   {
     duration: "4-5 nights",
     value: 0,
-    color: "#84cc16",
+    fill: "#84cc16",
   },
   {
     duration: "6-7 nights",
     value: 0,
-    color: "#22c55e",
+    fill: "#22c55e",
   },
   {
     duration: "8-14 nights",
     value: 0,
-    color: "#14b8a6",
+    fill: "#14b8a6",
   },
   {
     duration: "15-21 nights",
     value: 0,
-    color: "#3b82f6",
+    fill: "#1e59b6",
   },
   {
     duration: "21+ nights",
     value: 0,
-    color: "#a855f7",
+    fill: "#a855f7",
   },
 ];
 
@@ -65,48 +69,46 @@ const startDataDark = [
   {
     duration: "1 night",
     value: 0,
-    color: "#b91c1c",
+    fill: "#b91c1c",
   },
   {
     duration: "2 nights",
     value: 0,
-    color: "#c2410c",
+    fill: "#c2410c",
   },
   {
     duration: "3 nights",
     value: 0,
-    color: "#a16207",
+    fill: "#a16207",
   },
   {
     duration: "4-5 nights",
     value: 0,
-    color: "#4d7c0f",
+    fill: "#4d7c0f",
   },
   {
     duration: "6-7 nights",
     value: 0,
-    color: "#15803d",
+    fill: "#15803d",
   },
   {
     duration: "8-14 nights",
     value: 0,
-    color: "#0f766e",
+    fill: "#0f766e",
   },
   {
     duration: "15-21 nights",
     value: 0,
-    color: "#1d4ed8",
+    fill: "#1d4ed8",
   },
   {
     duration: "21+ nights",
     value: 0,
-    color: "#7e22ce",
+    fill: "#7e22ce",
   },
 ];
 
 function prepareData(startData, stays) {
-  // A bit ugly code, but sometimes this is what it takes when working with real data 😅
-
   function incArrayValue(arr, field) {
     return arr.map((obj) =>
       obj.duration === field ? { ...obj, value: obj.value + 1 } : obj
@@ -130,3 +132,41 @@ function prepareData(startData, stays) {
 
   return data;
 }
+
+function DurationChart({ confirmedStays }) {
+  const { isDarkMode } = useDarkMode();
+  const startData = isDarkMode ? startDataDark : startDataLight;
+
+  const data = prepareData(startData, confirmedStays);
+
+  return (
+    <ChartBox>
+      <Heading as="h2">Stay duration summary</Heading>
+      <ResponsiveContainer width="100%" height={240}>
+        <PieChart>
+          <Pie
+            data={data}
+            nameKey="duration"
+            dataKey="value"
+            innerRadius={85}
+            outerRadius={110}
+            cx="40%"
+            cy="50%"
+            paddingAngle={3}
+          />
+          <Tooltip />
+          <Legend
+            verticalAlign="middle"
+            align="right"
+            width="30%"
+            layout="vertical"
+            iconSize={15}
+            iconType="circle"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartBox>
+  );
+}
+
+export default DurationChart;
