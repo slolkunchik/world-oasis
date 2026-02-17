a1f5cc8b2b9245a521c3207db9e2b36bbaaa33f0
Marina Sevkovich <slolkunchik@gmail.com>
2026-02-17 14:57:25 +0300

    feat: add dashboard with sales chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,42 @@
 import styled from "styled-components";
 
+import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isPending: isLoading1 } = useRecentBookings();
+  const { isPending: isLoading2, confirmedStays, numDays } = useRecentStays();
+  const { isPending: isLoading3, cabins } = useCabins();
+
+  if (isLoading1 || isLoading2 || isLoading3) {
+    return <Spinner />;
+  }
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      ></Stats>
+      <div>Today's activity</div>
+      <div>Chart stay durations</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
