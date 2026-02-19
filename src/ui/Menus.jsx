a0aa24901a460a95c83c6db1f033a07f5453cd88
Marina Sevkovich <slolkunchik@gmail.com>
2026-02-19 13:19:18 +0300

    fix: menu toggle button double click behaviour

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -86,6 +86,8 @@ function Toggle({ id }) {
   const { open, close, openId, setPosition } = useContext(MenuContext);
 
   function handleClick(e) {
+    e.stopPropagation();
+
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -104,7 +106,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenuContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) {
     return null;
